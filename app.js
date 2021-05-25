1fa4dc6045ff62312cb970d554191bf3e9b4a40d
laura mills <littlemissuk80@gmail.com>
2021-05-25 13:48:16 +0100

    added typing effect to banner text using js

---
+++ app.js
@@ -0,0 +1,15 @@
+const texts = ['My name is Laura Mills'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+    if(count === texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    document.querySelector('.typing').textContent = letter;
+    setTimeout(type, 150);
+}());
